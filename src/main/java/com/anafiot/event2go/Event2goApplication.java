894b54268be1a4873b94fb43f17282facc55b8f3
Dmitriy Chalienko <dmitriy.chalienko@gmail.com>
2018-12-03 22:49:02 +0200

    Initial commit

---
+++ src/main/java/com/anafiot/event2go/Event2goApplication.java
@@ -0,0 +1,12 @@
+package com.anafiot.event2go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Event2goApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Event2goApplication.class, args);
+    }
+}
